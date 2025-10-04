4b8f61a678bd5ddcc83fc2c047fd7752814a9761
lando <orlandroide@gmail.com>
2025-10-03 17:42:27 -0700

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.assertlog("Hello DevOps!")
+console.log("I am learning CI/CD using Github Actions")
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Starting tests...")
+setTimeout(() => console.log("Waiting for tests"), 3000);
+console.log("Tests complete!")
